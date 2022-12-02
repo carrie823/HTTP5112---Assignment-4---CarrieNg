02402eb9e24bc54e7cff9e215c87d2a7325db5cc
carrie823 <me.carrieng@gmail.com>
2022-12-02 16:37:55 -0500

    a4 submission

---
+++ HTTP5112-Assignment3-CarrieNg/HTTP5112-Assignment3-CarrieNg/Controllers/TeacherController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HTTP5112_Assignment3_CarrieNg.Models;
+
+
+namespace HTTP5112_Assignment3_CarrieNg.Controllers
+{
+    public class TeacherController : Controller
+    {
+        // GET: Teacher
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //GET: /Teacher/List
+        public ActionResult List(string SearchKey = null)
+        {
+            TeacherDataController controller = new TeacherDataController();
+            IEnumerable<Teacher> Teachers = controller.ListTeachers(SearchKey);
+            return View(Teachers);
+        }
+
+        //GET : /Teacher/Show/{id}
+        public ActionResult Show(int id)
+        {
+            TeacherDataController controller = new TeacherDataController();     
+            Teacher NewTeacher = controller.FindTeacher(id);
+          
+
+            return View(NewTeacher);
+        }
+
+        //GET : /Teacher/DeleteConfirm/{id}
+        public ActionResult DeleteConfirm(int id)
+        {
+            TeacherDataController controller = new TeacherDataController();
+            Teacher NewTeacher = controller.FindTeacher(id);
+
+
+            return View(NewTeacher);
+        }
+
+        //POST : /Teacher/Delete/{id}
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            TeacherDataController controller = new TeacherDataController();
+            controller.DeleteTeacher(id);
+            return RedirectToAction("List");
+        }
+
+        //GET : /Teacher/New
+        public ActionResult New()
+        {
+            return View();
+        }
+
+        //POST : /Teacher/Create
+        [HttpPost]
+        public ActionResult Create(string TeacherFname, string TeacherLname, DateTime HireDate, string EmployeeNumber, double Salary)
+        {
+            //Identify that this method is running
+            //Identify the input provided from the form
+
+            Debug.WriteLine("I have accessed the create method");
+            Debug.WriteLine(TeacherFname);
+            Debug.WriteLine(TeacherLname);
+            Debug.WriteLine(HireDate);
+            Debug.WriteLine(EmployeeNumber);
+            Debug.WriteLine(Salary);
+
+            Teacher NewTeacher = new Teacher();
+            NewTeacher.TeacherFname = TeacherFname;
+            NewTeacher.TeacherLname = TeacherLname;
+            NewTeacher.HireDate = HireDate;
+            NewTeacher.EmployeeNumber = EmployeeNumber;
+            NewTeacher.Salary = Salary;
+
+            TeacherDataController controller = new TeacherDataController();
+            controller.AddTeacher(NewTeacher);
+
+
+            return RedirectToAction("List");
+        }
+    }
+}
---
+++ HTTP5112-Assignment3-CarrieNg/HTTP5112-Assignment3-CarrieNg/Controllers/TeacherDataController.cs
@@ -0,0 +1,185 @@
+﻿using HTTP5112_Assignment3_CarrieNg.Models;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+
+namespace HTTP5112_Assignment3_CarrieNg.Controllers
+{
+    public class TeacherDataController : ApiController
+    {
+        private SchoolDbContext school = new SchoolDbContext();
+
+        /// <summary>
+        /// contacts the database and returns articles in the teachers table. Search Bar can be used to seach for specific teachers by their first and lastnames
+        /// </summary>
+        /// <example> 
+        /// GET: api/TeacherData/ListTeachers/{SearchKey?} -> "Linda Chan"
+        /// </example>
+        /// <returns>
+        /// a list of all the teachers first and last names, when you select each teacher you can see all information of teacher from database
+        /// </returns>
+
+        [HttpGet]
+        [Route("api/TeacherData/ListTeachers/{SearchKey?}")]
+        public IEnumerable<Teacher> ListTeachers(string SearchKey=null)
+        {
+            //Create a connection
+            MySqlConnection Conn = school.AccessDatabase();
+
+            //Open the connection between the webserver and database -> School database
+            Conn.Open();
+
+            //make a command/query for the database
+            MySqlCommand cmd = Conn.CreateCommand();
+
+            //SQL QUERY
+            cmd.CommandText = "Select * from teachers where lower(teacherfname) like lower(@key) or lower(teacherlname) like lower(@key) or lower(concat (teacherfname, ' ', teacherlname)) like lower(@key)";
+
+            cmd.Parameters.AddWithValue("@key", "%" + SearchKey + "%");
+            cmd.Prepare();
+
+            //Gather Result Set of Query into a variable
+            MySqlDataReader ResultsSet = cmd.ExecuteReader();
+
+            //Create an empty list of teacher Names
+            List<Teacher> Teachers = new List<Teacher>();
+
+            //Loop through each row the Result Set
+            while (ResultsSet.Read())
+            {
+                int TeacherId = (int)ResultsSet["teacherid"];
+                string TeacherFname = (string)ResultsSet["teacherfname"];
+                string TeacherLname = (string)ResultsSet["teacherlname"];
+                string EmpolyeeNumber = (string)ResultsSet["employeenumber"];
+                double Salary = Double.Parse((ResultsSet["salary"].ToString()));
+                
+                DateTime HireDate = (DateTime)ResultsSet["hiredate"];
+
+                Teacher NewTeacher = new Teacher();
+                NewTeacher.TeacherId = TeacherId;
+                NewTeacher.TeacherFname = TeacherFname;
+                NewTeacher.TeacherLname = TeacherLname;
+                NewTeacher.EmployeeNumber = EmpolyeeNumber;
+                NewTeacher.Salary = Salary;
+                NewTeacher.HireDate = HireDate;
+                //string TeacherName = ResultsSet["teacherfname"] + " " + ResultsSet["teacherlname"];
+
+                Teachers.Add(NewTeacher);
+
+            }
+
+            //Close the connection between the MySQL Database and the WebServer
+            Conn.Close();
+
+            //Return the final list of teachers
+            return Teachers;
+
+        }
+        [HttpGet]
+        public Teacher FindTeacher(int id)
+        {
+            Teacher NewTeacher = new Teacher();
+
+            //Create a connection
+            MySqlConnection Conn = school.AccessDatabase();
+
+            //Open the connection between the webserver and database
+            Conn.Open();
+
+            //make a command/query for our database
+            MySqlCommand cmd = Conn.CreateCommand();
+
+            //SQL QUERY
+            cmd.CommandText = "Select * from teachers where teacherid ="+id;
+
+            //Gather Result Set of Query into a variable
+            MySqlDataReader ResultsSet = cmd.ExecuteReader();
+
+            //Loop through each row the Result Set
+            while (ResultsSet.Read())
+            {
+                int TeacherId = (int)ResultsSet["teacherid"];
+                string TeacherFname = (string)ResultsSet["teacherfname"];
+                string TeacherLname = (string)ResultsSet["teacherlname"];
+                string EmpolyeeNumber = (string)ResultsSet["employeenumber"];
+                double Salary = Double.Parse((ResultsSet["salary"].ToString()));
+                DateTime HireDate = (DateTime)ResultsSet["hiredate"];
+
+                NewTeacher.TeacherId = TeacherId;
+                NewTeacher.TeacherFname = TeacherFname;
+                NewTeacher.TeacherLname = TeacherLname;
+                NewTeacher.EmployeeNumber = EmpolyeeNumber;
+                NewTeacher.Salary = Salary;
+                NewTeacher.HireDate = HireDate;
+            }
+
+            //Return the final list of teachers
+            return NewTeacher;
+
+        }
+        /// <summary>
+        /// Delete a Teacher from our database
+        /// </summary>
+        /// <param name="id"></param>
+        /// <example>POST : /api/TeacherData/DeleteTeacher/3/</example>
+        [HttpPost]
+        public void DeleteTeacher(int id)
+        {
+            //create an instance of connection
+            MySqlConnection conn = school.AccessDatabase();
+
+            //open the connection between the web server and the database
+            conn.Open();
+
+            //Establish a new command (query) for our database
+            MySqlCommand cmd = conn.CreateCommand();
+
+            //Sql Query
+            cmd.CommandText = "Delete from teachers where teacherid=@id";
+            cmd.Parameters.AddWithValue("@id", id);
+            cmd.Prepare();
+
+            cmd.ExecuteNonQuery();
+
+            conn.Close();
+
+        }
+
+        /// <summary>
+        /// Add a Teacher from our database
+        /// </summary>
+        /// <example>inputing teacher first and lastname, employee number, hire date, salary into form.
+        /// and teacher will be added to database and their name will be visiable in Teacher/List/</example>
+
+        [HttpPost]
+        public void AddTeacher([FromBody]Teacher NewTeacher)
+        {
+            //create an instance of connection
+            MySqlConnection conn = school.AccessDatabase();
+
+            //open the connection between the web server and the database
+            conn.Open();
+
+            //Establish a new command (query) for our database
+            MySqlCommand cmd = conn.CreateCommand();
+
+            //Sql Query
+            cmd.CommandText = "Insert into teachers (teacherfname, teacherlname, hiredate, employeenumber, salary) values (@TeacherFname, @TeacherLname, @HireDate, @EmployeeNumber, @Salary)";
+            cmd.Parameters.AddWithValue("@TeacherFname", NewTeacher.TeacherFname);
+            cmd.Parameters.AddWithValue("@TeacherLname", NewTeacher.TeacherLname);
+            cmd.Parameters.AddWithValue("@EmployeeNumber", NewTeacher.EmployeeNumber);
+            cmd.Parameters.AddWithValue("@HireDate", NewTeacher.HireDate);
+            cmd.Parameters.AddWithValue("@Salary", NewTeacher.Salary);
+            cmd.Prepare();
+
+            cmd.ExecuteNonQuery();
+
+            conn.Close();
+        }
+    }
+}
